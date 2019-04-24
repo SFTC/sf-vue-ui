1a57c0ed1854c71bae5543214e632ce686ddb9f0
haoli <haoli23456@sf-express.com>
2019-04-24 15:29:03 +0800

    修改一下名称

--- packages/multi-echarts/index.js
+++
@@ -1,7 +0,0 @@
-import MultiEchart from "./src/MultiEchart";
-/* istanbul ignore next */
-MultiEchart.install = function(Vue) {
-  Vue.component(MultiEchart.name, MultiEchart);
-};
-
-export default MultiEchart;
