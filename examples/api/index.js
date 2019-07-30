34cd7483c1e17c76afe6ff85b4d71b7a3c7e80b3
赵占海 <zhaozhanhai@zhaozhanhaideMacBook-Pro-2.local>
2019-07-30 18:41:50 +0800

    pull master

--- examples/api/index.js
+++ examples/api/index.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 export const searchKnight = params =>
-  Vue.axios.get('http://10.188.60.215:7300/mock/5c00d99595b2682cbab89553/rider/user/account/getriderlist', { params }).then(res => res.data)
+  Vue.axios.get('https://easy-mock.com/mock/5ab7333f2f46280e4ecd4814/mocksf/user/account/getriderlist', { params }).then(res => res.data)
 
--- examples/router.js
+++ examples/router.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import InstallAtion from './pages/InstallAtion'
+import QuickStart from './pages/QuickStart'
 import FormTable from './pages/FormTable'
 import MultiSelect from './pages/MultiSelect'
 
@@ -7,6 +9,18 @@ Vue.use(Router)
 
 const router = new Router({
   routes: [
+    {
+      path: '/',
+      component: InstallAtion
+    },
+    {
+      path: '/installation',
+      component: InstallAtion
+    },
+    {
+      path: '/quickstart',
+      component: QuickStart
+    },
     {
       path: '/formtable',
       component: FormTable
